7a19d00ee64830a78c17335172527f18fd0951fa
Robert Smith <r2theob23@gmail.com>
2013-05-23 20:20:52 -0400

    Added the four functions to the top of the view controller.m file

--- AOC_Week3/AOC_Week3/ViewController.m
+++ AOC_Week3/AOC_Week3/ViewController.m
@@ -14,13 +14,53 @@
 
 @implementation ViewController
 
+//Add Function
+
+-(NSInteger)addOne:(NSInteger)x1 addTwo:(NSInteger)x2;
+{
+    return x1 + x2;
+}
+
+//Compare Function
+
+-(BOOL)compareOne:(NSInteger)y1 compareTwo:(NSInteger)y2;
+{
+
+    if(y1 == y2) {
+        return YES;
+    }else {
+        return NO;
+    }
+
+
+}
+
+//Append Two Strings Together
+
+- (NSString *)appendOne:(NSString *)z1 appendTwo:(NSString *)z2
+{
+    NSMutableString *myString = [NSMutableString stringWithString:z1];
+    [myString appendString:z2];
+    return myString;
+}
+
+//displayAlertWithString function
+
+-(void)displayAlertWithString:(NSString*)string; {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Hello" message:string delegate:self cancelButtonTitle:@"World" otherButtonTitles:nil, nil];
+    [alert show];
+
+
+}
 
 
 
 - (void)viewDidLoad
 {
     
- 
+    // Calling Addition Function
+    int result = [self addOne:14 addTwo:27];
+    [
  
 
     
