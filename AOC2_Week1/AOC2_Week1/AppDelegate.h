52fc94c8cad32082195bb5e917172699afc5ec42
Robert Smith <r2theob23@gmail.com>
2013-06-06 18:28:21 -0400

    Added new project to repo to begin week 1 of AOC 2

---
+++ AOC2_Week1/AOC2_Week1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC2_Week1
+//
+//  Created by Robert Smith on 6/6/13.
+//  Copyright (c) 2013 Robert Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC2_Week1/AOC2_Week1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC2_Week1
+//
+//  Created by Robert Smith on 6/6/13.
+//  Copyright (c) 2013 Robert Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
